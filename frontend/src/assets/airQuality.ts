57ccf2f9e20bac13760a60a009195dea2acbf93c
JuHyeong424 <144659958+JuHyeong424@users.noreply.github.com>
2025-06-04 17:54:13 +0900

    Update airQuality.ts

--- frontend/src/assets/airQuality.ts
+++ frontend/src/assets/airQuality.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AirQualityRow } from '../types/AirQuality.d.ts';
 
 const API_KEY = import.meta.env.VITE_AIR_QUALITY_API_KEY;
-const BASE_URL = 'http://openAPI.seoul.go.kr:8088';
+const BASE_URL = 'https://openAPI.seoul.go.kr:8088';
 
 export const fetchAirQuality = async (year: string): Promise<AirQualityRow[]> => {
     const url = `${BASE_URL}/${API_KEY}/json/YearlyAverageAirQuality/1/1000/${year}`;
